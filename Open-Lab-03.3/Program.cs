40bf352f300f0bee36bbdeec961aa9043771df8e
matejslovak <joggurtik1@gmail.com>
2019-11-07 08:45:01 +0100

    03.3

---
+++ Open-Lab-03.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Open_Lab_03._3
+{
+    class Program
+    {
+        public static bool CompareCharactersCount(string str1, string str2)
+        {
+            if (str1.Length == str2.Length)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First string");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Second string");
+            string str2 = Console.ReadLine();
+            Console.WriteLine(CompareCharactersCount(str1, str2));
+        }
+    }
+}
